1c2c2857058f54a22d140886c0880f09c61a8631
Will Smith <william.e.smithiv@gmail.com>
2021-04-07 08:08:35 -0400

    rename file & update

---
+++ beatties.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+from matplotlib import pyplot as plt
+import pydeck as pdk
+
+import streamlit.components.v1 as components
+
+st.title('Beatties Ford Road Project')
+
+
+st.video('https://www.youtube.com/watch?v=sK-MlWm7Iuc')
+
+
+grocery = pd.read_csv('./grocery-data/Grocery_Stores.csv')
+grocery = grocery.rename(columns={'X': 'longitude', 'Y': 'latitude'})
+
+colNames = grocery.columns
+
+st.write("Hello world")
+st.write(colNames)
+
+coords = grocery[['longitude', 'latitude']]
+avgLong = grocery['longitude'].median()
+avgLat = grocery['latitude'].median()
+
+st.write("""## Grocery stores in Mecklenburg County""")
+st.pydeck_chart(pdk.Deck(
+     map_style='mapbox://styles/mapbox/light-v9',
+     initial_view_state=pdk.ViewState(
+         latitude=35.285122,
+         longitude=-80.846734,
+         zoom=9.5,
+         pitch=0,
+     ),
+     layers=[
+         pdk.Layer(
+             'ScatterplotLayer',
+             data=coords,
+             get_position=['longitude','latitude'],
+             get_color='[200, 30, 0, 160]',
+             get_radius=200,
+         ),
+     ],
+))
+
+components.html("""
+<iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.socialexplorer.com/bc9c206f28/embed" width="640" height="480" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>
+""", width=640,height=480)
+
+components.html("""
+    <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.socialexplorer.com/7e100dcc9b/embed" width="640" height="480" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>
+""", width=640,height=480)
+
+
