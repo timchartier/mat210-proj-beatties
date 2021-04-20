2b9ffab1fc65663e0544f33e3884001ce62be133
Will Smith <william.e.smithiv@gmail.com>
2021-04-20 07:53:31 -0400

    testing from pickled data

---
+++ testing.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import plotly.express as px
+import plotly.figure_factory as ff
+import pandas as pd
+import numpy as np
+import os
+
+
+dataFrames = {
+    'Character': 'character',
+    'Economy': 'economy',
+    'Education': 'education',
+    'Engagement': 'engagement',
+    'Environment': 'environment',
+    'Health': 'health',
+    'Housing': 'housing',
+    'Safety': 'safety',
+    'Transportation': 'transportation'
+}
+
+def getDataFrame(topic):
+    return pd.read_pickle('./qol-data/pickles/{}.pkl'.format(topic))
+
+
+df = getDataFrame(dataFrames['Economy'])
+df[['Household_Income_2017','Household_Income_2018']] = df[['Household_Income_2017','Household_Income_2018']].astype(float)
+
+
+st.write(df)
