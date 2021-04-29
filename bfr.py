94d30e59e93a588f7b6f1f4aa06296bfcd354a4e
lcarr14 <54549127+lcarr14@users.noreply.github.com>
2021-04-29 14:41:44 -0400

    added timedata to sidebar

--- bfr.py
+++ bfr.py
@@ -7,6 +7,7 @@ import subpages.beatties as beatties
 import subpages.raceOverTime as raceOverTime
 import subpages.evictions as evictions
 import subpages.testing as journey
+import subpages.quick_history as history
 # This is the master file for the Beatties Ford Road dashboard
 st.set_page_config(page_title="Beatties Ford Road", layout="wide")
 
@@ -17,6 +18,7 @@ PAGES = {
     'Journey Down Beatties Ford Road': journey,
     'Clustering': clustering,
     'Beatties': beatties,
+    'Data Over Time' : history,
     'Demographics over Time': raceOverTime,
     'Evictions': evictions
 }
